6084479ce0626fea667623b80b4fb41e86dd4fd5
Zachary Stence <zachstence@gmail.com>
2021-10-30 16:42:48 -0500

    refactor: split app out of index

---
+++ server/src/app.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import Database from "./Database";
+
+const db = new Database();
+
+const app = express();
+app.use(bodyParser.text());
+app.use(cors({
+    origin: "http://localhost:8080"
+}));
+
+const URL_REGEX = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+
+app.post("/add", (req, res) => {
+    const url = req.body as string;
+
+    if (url.match(URL_REGEX)) {
+        const entry = db.add(url);
+        res.send(entry.id);
+    } else {
+        res.status(400);
+        res.send("Malformed URL");
+    }
+});
+
+app.get("/:id", (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const entry = db.get(id);
+        res.send(entry.url);
+    } catch (e) {
+        res.status(404);
+        res.send("ID not found");
+    }
+});
+
+export default app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,45 +1,7 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
+import app from "./app";
 
-import Database from "./Database";
+const PORT = 8081;
 
-const db = new Database();
-
-const app = express();
-app.use(bodyParser.text());
-app.use(cors({
-    origin: "http://localhost:8080"
-}));
-
-const port = 8081;
-
-const URL_REGEX = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
-
-app.post("/add", (req, res) => {
-    const url = req.body as string;
-
-    if (url.match(URL_REGEX)) {
-        const entry = db.add(url);
-        res.send(entry.id);
-    } else {
-        res.status(400);
-        res.send("Malformed URL");
-    }
-});
-
-app.get("/:id", (req, res) => {
-    const {id} = req.params;
-
-    try {
-        const entry = db.get(id);
-        res.send(entry.url);
-    } catch (e) {
-        res.status(404);
-        res.send("ID not found");
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server listening on at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server listening on at http://localhost:${PORT}`);
 });
