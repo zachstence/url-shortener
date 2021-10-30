14239f827679481e7cc79d45c4dcce7c9ffec85d
Zachary Stence <zachstence@gmail.com>
2021-10-30 15:28:14 -0500

    feat: Redirect component

---
+++ ui/src/Redirect.tsx
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+const Redirect: React.FC = () => {
+    const [isError, setIsError] = useState<boolean>(false);
+
+    const {id} = useParams<{id: string}>();
+
+    useEffect(() => {
+        let response: AxiosResponse;
+        (async () => {
+            try {
+                response = await axios.get(`http://localhost:8081/${id}`);
+                window.location.href = response.data;
+            } catch {
+                setIsError(true);
+            }
+        })();
+    }, []);
+
+    if (isError) {
+        return <div>Error</div>;
+    } else {
+        return <div>Loading...</div>;
+    }
+};
+
+export default Redirect;
