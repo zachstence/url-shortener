612efc0fb0a673f9168d1216db86e1202cfd7d56
Zachary Stence <zachstence@gmail.com>
2021-10-31 10:18:49 -0500

    feat: add placeholder to input

--- ui/src/components/Shorten/Shorten.tsx
+++ ui/src/components/Shorten/Shorten.tsx
@@ -22,7 +22,14 @@ const Shorten: React.FC = () => {
         <div className="shorten">
             <div className="input-group">
                 <label htmlFor="url">URL</label>
-                <input id="url" className="gradient-border" type="text" value={url} onChange={e => setUrl(e.target.value)} />
+                <input
+                    id="url"
+                    className="gradient-border"
+                    type="text"
+                    value={url}
+                    placeholder="https://www.website.com/with/a/long/url"
+                    onChange={e => setUrl(e.target.value)}
+                />
             </div>
             <button type="button" onClick={onClick}>SHORTEN</button>
             <a href={short} className="short">{short}</a>
